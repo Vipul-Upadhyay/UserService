8c1ba710ddabf9459402a197ee8df479582ba22c
Vipul-Upadhyay <upa.vipul@gmail.com>
2024-03-15 22:16:52 +0530

    SignUp and Logging

---
+++ src/main/java/com/userservice/userservice/repositories/SessionRepository.java
@@ -0,0 +1,14 @@
+package com.userservice.userservice.repositories;
+
+import com.userservice.userservice.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+    Optional<Session> findByTokenAndUser_Id(String token, Long userId);
+    //select * from sessions where token = <> and userId = <>
+}
